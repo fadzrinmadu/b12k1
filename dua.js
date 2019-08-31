5e3b418e6f130c76425fad8a398dfac60f753112
Muhammad Fadzrin Madu <aan.fadzrin@gmail.com>
2019-08-31 07:49:06 -0700

    Update dua.js

--- dua.js
+++ dua.js
@@ -1,7 +1,14 @@
 function is_username_valid(username) {
-	let regexp = /^[^0-9\[\]\?\/<~#.\!@\$%\^\&\*\(\)\+\=\{\}:";'\,>]{1}[a-zA-Z0-9]{5,9}/;
+	let regexp = /^[^\b0-9\s\[\]\?\/\<\~\#\'\!\@\$\%\^\&\*\(\)\+\=\}\|\:\"\;\'\,\>\}][a-zA-Z0-9]{5,8}$/
 	return regexp.test(username);
 }
 
-console.log(is_username_valid('A+Xrutaf888')); // true ,
+function is_password_valid(password) {
+	let regexp = /[a-zA-Z0-1]\=/;
+	return regexp.test(password);
+}
+
+console.log(is_username_valid('Xrutaf888')); // true
 console.log(is_username_valid('1Diana')); // false
+console.log(is_password_valid('passW0rd=')); // true
+console.log(is_password_valid('C0d3YourFuture!#')); // false
